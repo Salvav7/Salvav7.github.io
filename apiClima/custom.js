d7e62203f3106a7f387832b2b1c7ac1cd77aece0
Salvav <villegassalva94@gmail.com>
2023-04-11 16:16:41 -0600

    up

--- apiClima/custom.js
+++ apiClima/custom.js
@@ -1,4 +1,4 @@
-iconst result = document.querySelector('.result');
+const result = document.querySelector('.result');
 const form = document.querySelector('.get-weather');
 const nameCity = document.querySelector('#city');
 const nameCountry = document.querySelector('#country');
